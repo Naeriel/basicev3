8e8565f2898d7b71fca6119c63b685b370d43f88
Cristina <serranog.cristina@gmail.com>
2017-07-12 13:24:32 +0200

    Added while

--- printLCD.py
+++ printLCD.py
@@ -1,6 +1,7 @@
 import ev3dev.ev3 as ev3
 import ev3dev.fonts as fonts
 
-lcd = ev3.Screen()
-lcd.update()
-lcd.draw.text((10,10), 'Hello World!', font=fonts.load("luBS14"))
+while True
+    lcd = ev3.Screen()
+    lcd.update()
+    lcd.draw.text((10,10), 'Hello World!', font=fonts.load("luBS14"))
