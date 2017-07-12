a9fb7ca6a6e5ca748c0633c99408254be8cbbeac
Cristina <serranog.cristina@gmail.com>
2017-07-12 14:28:08 +0200

    LCD prints "Hello World"

--- printLCD.py
+++ printLCD.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
+
 import ev3dev.ev3 as ev3
 import ev3dev.fonts as fonts
 from time import sleep
 
+# Load Screen
 lcd = ev3.Screen()
-lcd.draw.text((10,10), 'Hello World!', font=fonts.load("luBS14"))
+lcd.draw.text((10,10), 'My name is Cristina\n I am an engineer', font=fonts.load("luBS14"))
 lcd.update()
 sleep(2)
