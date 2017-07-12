5977530a9d38588a2ea6c6541d67692a51f3f42b
Cristina <serranog.cristina@gmail.com>
2017-07-12 13:35:17 +0200

    Removed while

--- printLCD.py
+++ printLCD.py
@@ -1,8 +1,9 @@
 import ev3dev.ev3 as ev3
 import ev3dev.fonts as fonts
 from time import sleep
-while True:
-    lcd = ev3.Screen()
-    lcd.update()
-    lcd.draw.text((10,10), 'Hello World!', font=fonts.load("luBS14"))
-    sleep(2)
+
+lcd = ev3.Screen()
+lcd.update()
+lcd.draw.text((10,10), 'Hello World!', font=fonts.load("luBS14"))
+lcd.update()
+sleep(2)
