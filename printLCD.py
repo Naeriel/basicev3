feeea51713610a56a6e9f662372dba6d276c5878
Cristina <serranog.cristina@gmail.com>
2017-07-12 14:31:12 +0200

    LCD prints greetings

--- printLCD.py
+++ printLCD.py
@@ -8,6 +8,6 @@ from time import sleep
 # Load Screen
 lcd = ev3.Screen()
 lcd.draw.text((10,10), 'My name is Cristina', font=fonts.load("luBS14"))
-lcd.draw.text((20,10), 'I am an engineer', font=fonts.load("luBS14"))
+lcd.draw.text((10,20), 'I am an engineer', font=fonts.load("luBS14"))
 lcd.update()
 sleep(2)
