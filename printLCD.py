4b672ec2d39a7d030167736c57e7a17771df385e
Cristina <serranog.cristina@gmail.com>
2017-07-12 13:23:13 +0200

    Added update()

--- printLCD.py
+++ printLCD.py
@@ -2,4 +2,5 @@ import ev3dev.ev3 as ev3
 import ev3dev.fonts as fonts
 
 lcd = ev3.Screen()
-lcd.draw.text((10,10), 'Hello World!', font=fonts.load('luBS14'))
+lcd.update()
+lcd.draw.text((10,10), 'Hello World!', font=fonts.load("luBS14"))
